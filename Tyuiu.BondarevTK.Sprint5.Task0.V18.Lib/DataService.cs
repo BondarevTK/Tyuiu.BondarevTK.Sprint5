635b4363d6477219588cffeb2816500a1a4b8358
BondarevTK <BondarevTK101@gmail.com>
2024-12-18 13:42:36 +0300

    Add project files.

---
+++ Tyuiu.BondarevTK.Sprint5.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BondarevTK.Sprint5.Task0.V18.Lib
+{
+    public class DataService : ISprint5Task0V18
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.GetTempFileName();
+            double z = x / Math.Sqrt(Math.Pow(x,2) + x);
+            z = Math.Round(z, 2);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
