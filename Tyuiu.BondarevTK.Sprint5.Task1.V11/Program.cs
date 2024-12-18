30fc7afc72c5edb1e8a8c51c80bb3afc0ee49262
BondarevTK <BondarevTK101@gmail.com>
2024-12-18 15:26:52 +0300

    t1

--- Tyuiu.BondarevTK.Sprint5.Task1.V11/Program.cs
+++ Tyuiu.BondarevTK.Sprint5.Task1.V11/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Tyuiu.BondarevTK.Sprint5.Task1.V11
+﻿using Tyuiu.BondarevTK.Sprint5.Task1.V11.Lib;
+namespace Tyuiu.BondarevTK.Sprint5.Task1.V11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            int x = -5;
+            int y = 5;
+            string res = ds.SaveToFileTextData(x, y);
+            StreamReader sr = new StreamReader(res);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
         }
     }
 }
