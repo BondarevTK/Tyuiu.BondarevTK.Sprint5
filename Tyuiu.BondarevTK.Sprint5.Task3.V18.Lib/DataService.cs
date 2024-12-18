36f60c5ce17c7d183f27b966af7046d33caaf310
BondarevTK <BondarevTK101@gmail.com>
2024-12-18 17:06:10 +0300

    t3

---
+++ Tyuiu.BondarevTK.Sprint5.Task3.V18.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BondarevTK.Sprint5.Task3.V18.Lib
+{
+    public class DataService : ISprint5Task3V18
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.GetTempFileName();
+            double res = 2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x + 2;
+            res = Math.Round(res, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(res));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.BondarevTK.Sprint5.Task3.V18/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BondarevTK.Sprint5.Task3.V18.Lib;
+namespace Tyuiu.BondarevTK.Sprint5.Task3.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            string res = ds.SaveToFileTextData(x);
+            StreamReader sr = new StreamReader(res);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+        }
+    }
+}
