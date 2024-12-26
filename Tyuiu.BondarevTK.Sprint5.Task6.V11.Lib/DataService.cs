3327d1b2903bf88935d438b877b395ded29e22bc
BondarevTK <BondarevTK101@gmail.com>
2024-12-27 02:29:30 +0300

    T6

---
+++ Tyuiu.BondarevTK.Sprint5.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BondarevTK.Sprint5.Task6.V11.Lib
+{
+    public class DataService : ISprint5Task6V11
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            string text = File.ReadAllText(path);
+            string[] strings = text.Split(new char[] { ' ', ',', '?' });
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if ( strings[i].Length == 6)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+}
