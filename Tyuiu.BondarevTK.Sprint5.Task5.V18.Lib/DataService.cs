bfec3393feb876c5e1ea5b3bc8cdd5c505b10643
BondarevTK <BondarevTK101@gmail.com>
2024-12-27 22:05:24 +0300

    1

--- Tyuiu.BondarevTK.Sprint5.Task5.V18.Lib/DataService.cs
+++ Tyuiu.BondarevTK.Sprint5.Task5.V18.Lib/DataService.cs
@@ -10,12 +10,12 @@ namespace Tyuiu.BondarevTK.Sprint5.Task5.V18.Lib
             double res = 1;
             foreach (string s in strings)
             {
-                if (Double.TryParse(s.Trim(), out double number) && Math.Abs(number) > 9 && Math.Abs(number) < 100)
+                if (Double.TryParse(s, out double number) && Math.Abs(number) > 9 && Math.Abs(number) < 100)
                 {
                     res = res * number;
                 }
             }
-            return res;
+            return Math.Round(res,3);
         }
     }
 }
--- Tyuiu.BondarevTK.Sprint5.Task5.V18.Test/DataServiceTest.cs
+++ Tyuiu.BondarevTK.Sprint5.Task5.V18.Test/DataServiceTest.cs
@@ -11,5 +11,20 @@ namespace Tyuiu.BondarevTK.Sprint5.Task5.V18.Test
             bool fileExists = file.Exists;
             Assert.IsTrue(fileExists);
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string text = "13.93 7 -8 2 8 -6.61 19 11.88 -5 1 12.33 2.71 5 5.66 -7 14 -1.27 -2.49 7.23 3";
+            string[] strings = text.Split(" ");
+            double res = 1;
+            foreach (string s in strings)
+            {
+                if (Double.TryParse(s, out double number) && Math.Abs(number) > 9 && Math.Abs(number) < 100)
+                {
+                    res = res * number;
+                }
+            }
+            Assert.AreEqual(Math.Round(res,3), 542765.545);
+        }
     }
 }
