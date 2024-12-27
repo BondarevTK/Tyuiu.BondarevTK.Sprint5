64b8a47b809f8ea51df54746b3b7aa602e268c3a
BondarevTK <BondarevTK101@gmail.com>
2024-12-27 22:12:10 +0300

    1

--- Tyuiu.BondarevTK.Sprint5.Task4.V16.Lib/DataService.cs
+++ Tyuiu.BondarevTK.Sprint5.Task4.V16.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.BondarevTK.Sprint5.Task4.V16.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
-            double x = Convert.ToDouble(strx);
+            double x = Double.Parse(strx);
             double res = Math.Cos(x) + Math.Pow(x, 2) - 2 * (x / 1.2);
             res = Math.Round(res, 3);
             return res;
