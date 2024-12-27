634b4430b6419b4d7d86f7b815ebb184ca111dfa
BondarevTK <BondarevTK101@gmail.com>
2024-12-27 22:17:29 +0300

    t1

--- Tyuiu.BondarevTK.Sprint5.Task4.V16.Lib/DataService.cs
+++ Tyuiu.BondarevTK.Sprint5.Task4.V16.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.BondarevTK.Sprint5.Task4.V16.Lib
 {
     public class DataService : ISprint5Task4V16
@@ -6,7 +7,9 @@ namespace Tyuiu.BondarevTK.Sprint5.Task4.V16.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
-            double x = Double.Parse(strx);
+            var fmt = new NumberFormatInfo();
+            fmt.NegativeSign = "−";
+            var x = double.Parse(strx, fmt);
             double res = Math.Cos(x) + Math.Pow(x, 2) - 2 * (x / 1.2);
             res = Math.Round(res, 3);
             return res;
