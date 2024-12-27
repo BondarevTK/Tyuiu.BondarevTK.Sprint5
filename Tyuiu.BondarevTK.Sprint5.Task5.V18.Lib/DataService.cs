09b305b3fc02fe4542988ba914294dec4df554af
BondarevTK <BondarevTK101@gmail.com>
2024-12-27 20:27:21 +0300

    t7

--- Tyuiu.BondarevTK.Sprint5.Task5.V18.Lib/DataService.cs
+++ Tyuiu.BondarevTK.Sprint5.Task5.V18.Lib/DataService.cs
@@ -7,20 +7,13 @@ namespace Tyuiu.BondarevTK.Sprint5.Task5.V18.Lib
         {
             string text = File.ReadAllText(path);
             string[] strings = text.Split(",");
-            double[] num = new double[strings.Length];
             double res = 1;
-            int i = 0;
             foreach (string s in strings)
             {
                 if (int.TryParse(s.Trim(), out int number) && number > 9 && number < 100)
                 {
-                    num[i] = number;
-                    i += 1;
+                    res *= number;
                 }
-            }
-            for (int j = 0; j < num.Length; j++)
-            {
-                res *= num[j];
             }
             return res;
         }
